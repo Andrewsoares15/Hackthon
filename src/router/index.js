1d3062f2f9e46b80565cb01328e6b887087693d1
Andrew soares <andrewsoares347@gmail.com>
2021-10-02 11:55:40 -0300

    configurando rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Animes from '../views/Animes.vue'
+import Games from '../views/Games.vue'
+import Sobre from '../views/Animes.vue'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -8,6 +12,26 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/animes',
+    name: 'Animes',
+    component: Animes
+  },
+  {
+    path: '/sobre',
+    name: 'Sobre',
+    component: Sobre
+  },
+  {
+    path: '/games',
+    name: 'Games',
+    component: Games
+  },
+  {
+    path: '/games',
+    name: 'Games',
+    component: Games
   },
 ]
 
