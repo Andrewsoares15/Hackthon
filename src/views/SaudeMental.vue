5cbab47e0609fab128f12326bae7c5aaef729f17
Andrew soares <andrewsoares347@gmail.com>
2021-10-02 14:16:32 -0300

    ajuste tela de saude

--- src/views/SaudeMental.vue
+++ src/views/SaudeMental.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="titulo-container">>
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <h1>Lazer e Saúde Mental</h1>
-    <p align="justify">Você pode – e deve – procurar reservar ao menos uns minutinhos diários para atividades que sente prazer em fazer
+  <div class="saude">
+    <div class="titulo-container">
+      <h1>Lazer e Saúde Mental</h1>
+    </div>
+    <p>Você pode – e deve – procurar reservar ao menos uns minutinhos diários para atividades que sente prazer em fazer
 
 As atividades de lazer não apenas ajudam a controlar o estresse – sabemos que o estresse está ligado à piora do estado de saúde mental – como nos dão um senso de autocuidado e de equilíbrio, algo que ajuda a reduzir a ansiedade. Além, é claro, de melhorar a autoestima.
 
-Alguns estudos mostraram que os momentos que se reservam para fazer algo em benefício próprio ajudam a acelerar a recuperação de pessoas com transtornos mentais mais graves, como depressão.
+Alguns estudos mostraram que os momentos que se reservam para fazer algo em benefício próprio ajudam a acelerar a recuperação de pacientes com transtornos mentais mais graves, como depressão.
 
 O curioso é que, quando estamos estressados ou sobrecarregados de trabalho, geralmente a primeira coisa que sacrificamos é o nosso tempo livre.
 
@@ -19,9 +19,11 @@ As horas de lazer são a melhor válvula de descompressão, especialmente para e
 
 <script>
 
+
 export default {
   name: "SaudeMental",
   components: {
+   
   },
 };
 </script>
