b34450640087ca576bef40fc42a8be55d0046972
Andrew soares <andrewsoares347@gmail.com>
2021-10-02 12:53:13 -0300

    criei a pag inicial

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="inicio">
-    <h1>Inicio</h1>
+   
+    <h1 class="text-center">Games</h1>
+     
+    <p class="text-left ml-4">Games fazem parte de nossas vidas. Em todo momento estamos imersos em gamificação.  Educação, empresas, saúde e serviços utilizam de estratégias de games em seus negócios para engajar os consumidores.</p>
+    <v-img src="../assets/game.jpg" class="mt-5 mb-3 ml-3 mr-3" ></v-img>
+    <h2 class="text-center">Games na pandemia</h2>
+    <p class="text-left ml-4">Quando falamos sobre a pandemia, o cenário atual fez com que ocorresse uma explosão de usos. O grupo de pesquisa de mercado NPD, publicou o 2020 Gamer Segmentation Report, que é um levantamento onde foi concluído que três em cada quatro norte-americanos estão jogando videogame em casa desde o início de 2020, representando diante dos dois últimos anos, o acréscimo de 32 milhões de pessoas.</p>
   </div>
 </template>
 
@@ -13,3 +19,6 @@ export default {
   }
 }
 </script>
+<style scoped>
+
+</style>
