934767f947de19387eab5e7c2752d22e53d42fc2
Leonardo <leozikasantos73@gmail.com>
2021-10-02 12:03:31 -0300

    Criando funcção para receber os jogos da api

--- src/views/Games.vue
+++ src/views/Games.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="games">
     <img alt="Vue logo" src="../assets/logo.png">
-    
+    <h1>Games</h1>
   </div>
 </template>
 
@@ -16,7 +16,14 @@ export default {
     return {
       gamesLista: []
     }
+  },
+    created() {
+      fetch('https://it3zxc-default-rtdb.firebaseio.com/lazer/jogos.json')
+        .then(response => response.json())
+        .then(json => {
+          this.gamesLista = json;
+          console.log(this.gamesLista)
+        })
+    }
   }
-  
-}
 </script>
