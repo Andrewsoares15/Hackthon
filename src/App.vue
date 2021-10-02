58538540b620b0f71f37dab10bbc5dcf94af4270
Andrew soares <andrewsoares347@gmail.com>
2021-10-02 13:56:47 -0300

    criando o footer

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,22 @@
     <v-main>
      <router-view/>
     </v-main>
+    <Footer/>
   </v-app>
 </template>
 
 <script>
 import AppTopBar from './components/AppTopBar.vue'
 import { VLibras } from '@vue-a11y/vlibras'
+import Footer from './components/Footer.vue'
 export default {
   name: 'App',
   components:{
     AppTopBar,
-    VLibras
+    VLibras,
+    Footer
   }
 }
 </script>
-<style lang="scss">
-
+<style >
 </style>
