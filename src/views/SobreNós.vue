e321ebc09e82043f6ea61eed0ebf56f537e3f38c
Thais Cruvinel <thais.cruvinel@gmail.com>
2021-10-02 12:40:24 -0300

    atualizacao view

--- src/views/SobreNós.vue
+++ src/views/SobreNós.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="sobre">
-    <h1>This is an about page</h1>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>Sobre nós</h1>
+    <p>
+      Somos uma comunidade que além de amar cultura pop (especialmente animes e
+      jogos!) acredita que o lazer de forma geral é parte fundamental da nossa
+      saúde, principalmente nestes tempos de pandemia em que muitos de nós
+      sofremos com questões como ansiedade, fobia social, dentre outros. Aqui,
+      além de encontrar nossas sugestões de animes e jogos que amamos, você pode
+      também se conectar com outras pessoas que dividem essa mesma paixão,
+      trocar ideias e discutir sobre alguma coisa nova. Se quiser nos sugerir
+      algo que ainda não conste nas nossas listas, mande seu anime e/ou jogo no
+      nosso grupo do Telegram que em breve ela aparecerá aqui. Esperamos que
+      você se divirta com as nossas sugestões e conheça novos amigos!
+    </p>
   </div>
 </template>
+
+<script>
+export default {
+  name: "sobre",
+};
+</script>
+
+<style scoped></style>
