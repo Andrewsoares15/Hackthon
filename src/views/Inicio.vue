3bb00f76dbaac782d8de8191a74336bf50da509b
Leonardo <leozikasantos73@gmail.com>
2021-10-02 15:31:32 -0300

    adicionando  cards no inicio

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -3,10 +3,9 @@
    
     <h1 class="text-center">Games</h1>
      
-    <p class="ml-3 mr-3" align="justify">Games fazem parte de nossas vidas. Em todo momento estamos imersos em gamificação.  Educação, empresas, saúde e serviços utilizam de estratégias de games em seus negócios para engajar os consumidores.</p>
-    <v-img src="../assets/game.jpg" class="mt-5 mb-3 ml-3 mr-3 img" ></v-img>
+    <p class="text-left ml-4">Games fazem parte de nossas vidas. Em todo momento estamos imersos em gamificação.  Educação, empresas, saúde e serviços utilizam de estratégias de games em seus negócios para engajar os consumidores.</p>
+    <v-img src="../assets/game.jpg" class="mt-5 mb-3 ml-3 mr-3" ></v-img>
     <h2 class="text-center">Games na pandemia</h2>
-<<<<<<< HEAD
     <p class="text-left ml-4">Quando falamos sobre a pandemia, o cenário atual fez com que ocorresse uma explosão de usos. O grupo de pesquisa de mercado NPD, publicou o 2020 Gamer Segmentation Report, que é um levantamento onde foi concluído que três em cada quatro norte-americanos estão jogando videogame em casa desde o início de 2020, representando diante dos dois últimos anos, o acréscimo de 32 milhões de pessoas.</p>
       <v-card
         class="mx-auto"
@@ -61,9 +60,6 @@
           </v-row>
         </v-container>
       </v-card>
-=======
-    <p class="ml-3 mr-3" align="justify">Quando falamos sobre a pandemia, o cenário atual fez com que ocorresse uma explosão de usos. O grupo de pesquisa de mercado NPD, publicou o 2020 Gamer Segmentation Report, que é um levantamento onde foi concluído que três em cada quatro norte-americanos estão jogando videogame em casa desde o início de 2020, representando diante dos dois últimos anos, o acréscimo de 32 milhões de pessoas.</p>
->>>>>>> 85a380ee3bb63a456cb5716658d2d3cd732b298a
   </div>
 </template>
 
@@ -84,19 +80,5 @@ export default {
 }
 </script>
 <style scoped>
-@media only screen and (min-width: 600px) {
-  .img{
-    max-width: 60%;
-    display: block;
-    margin: 0 auto;
-    position: relative;
-    left: 20%;
-  }
-  p{
-    max-width: 800px;
-    position: relative;
-    left: 25%;
-  }
-  
-}
+
 </style>
