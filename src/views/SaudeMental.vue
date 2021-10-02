3ec29f96c72add25ce2f3d5d5ccd657f3974d603
Paulo Boldrini <84434822+PauloBoldrini@users.noreply.github.com>
2021-10-02 14:48:03 -0300

    imagem saude mental

--- src/views/SaudeMental.vue
+++ src/views/SaudeMental.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="saude">
-    <div class="titulo-container">
-      <h1>Lazer e Saúde Mental</h1>
-    </div>
-    <p>Você pode – e deve – procurar reservar ao menos uns minutinhos diários para atividades que sente prazer em fazer
+    <div>
+      <h1 align="center">Lazer e Saúde Mental</h1>
+    <center><img src="../assets/saude_mental.png" alt="some text" width=200 height=200 ></center>
+    
+    <p align="justify">Você pode – e deve – procurar reservar ao menos uns minutinhos diários para atividades que sente prazer em fazer
 
 As atividades de lazer não apenas ajudam a controlar o estresse – sabemos que o estresse está ligado à piora do estado de saúde mental – como nos dão um senso de autocuidado e de equilíbrio, algo que ajuda a reduzir a ansiedade. Além, é claro, de melhorar a autoestima.</p>
 
 
-    <p>Alguns estudos mostraram que os momentos que se reservam para fazer algo em benefício próprio ajudam a acelerar a recuperação de pacientes com transtornos mentais mais graves, como depressão.
+    <p align="justify">Alguns estudos mostraram que os momentos que se reservam para fazer algo em benefício próprio ajudam a acelerar a recuperação de pacientes com transtornos mentais mais graves, como depressão.
 
 O curioso é que, quando estamos estressados ou sobrecarregados de trabalho, geralmente a primeira coisa que sacrificamos é o nosso tempo livre.
 
 As horas de lazer são a melhor válvula de descompressão, especialmente para estes tempos ultravelozes das redes sociais, nos quais a pressão pelo sucesso é enorme e nos força a buscar sempre mais e, quando conseguimos, parece que nada mais faz sentido.</p> 
+    </div>
   </div>
 </template>
 
