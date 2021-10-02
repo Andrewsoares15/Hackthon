f8e88359a22eefe7b41e6d9551f8380a3f88a3aa
Paulo Boldrini <84434822+PauloBoldrini@users.noreply.github.com>
2021-10-02 13:00:30 -0300

    Atualizacao paragrafo saude mental

--- src/views/SaudeMental.vue
+++ src/views/SaudeMental.vue
@@ -3,6 +3,17 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <h1>Lazer e Saúde Mental</h1>
+    <p>Você pode – e deve – procurar reservar ao menos uns minutinhos diários para atividades que sente prazer em fazer
+
+As atividades de lazer não apenas ajudam a controlar o estresse – sabemos que o estresse está ligado à piora do estado de saúde mental – como nos dão um senso de autocuidado e de equilíbrio, algo que ajuda a reduzir a ansiedade. Além, é claro, de melhorar a autoestima.
+
+Alguns estudos mostraram que os momentos que se reservam para fazer algo em benefício próprio ajudam a acelerar a recuperação de pacientes com transtornos mentais mais graves, como depressão.
+
+O curioso é que, quando estamos estressados ou sobrecarregados de trabalho, geralmente a primeira coisa que sacrificamos é o nosso tempo livre.
+
+As horas de lazer são a melhor válvula de descompressão, especialmente para estes tempos ultravelozes das redes sociais, nos quais a pressão pelo sucesso é enorme e nos força a buscar sempre mais e, quando conseguimos, parece que nada mais faz sentido.
+
+</p>
   </div>
 </template>
 
